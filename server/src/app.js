6af510892a09b1383136964a4abb7aec6f94af67
Awais Aslam <awaisaslam336@gmail.com>
2022-06-17 18:15:11 +0500

    Serving FrontEnd (React.js) in Production

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
@@ -7,6 +8,7 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }))
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
 
 module.exports = app;
