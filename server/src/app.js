d19bf02ed2b343478c51528a4cf412dff0f819de
Awais Aslam <awaisaslam336@gmail.com>
2022-06-18 20:18:24 +0500

    Logging Requests Using Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require("express");
 const cors = require('cors');
+const morgan = require('morgan');
+
 const planetsRouter = require('./routes/planets/planets.router');
+
 const app = express();
 
+//security related middleware
 app.use(cors({
     origin: 'http://localhost:3000'
-}))
+}));
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
