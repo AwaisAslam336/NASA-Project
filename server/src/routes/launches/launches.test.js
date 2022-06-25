0a6b052dc6f46c2f0f47eddb112c537b7a9def43
Awais Aslam <awaisaslam336@gmail.com>
2022-06-25 15:22:52 +0500

    Testing API with jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,19 @@
+describe('Test GET /launches',()=>{
+    test('it should respond with 200 request',()=>{
+        const response = 200;
+        expect(response).toBe(200);
+    })
+})
+
+describe('Test POST /launches',()=>{
+    test('it should respond with 200 request', ()=>{
+        const response = 200;
+        expect(response).toBe(200);
+    });
+    test('it should catch missing required properties',()=>{
+
+    });
+    test('it should catch invalid dates',()=>{
+        
+    });
+})
