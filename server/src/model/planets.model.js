ac6b475fc069d90298fc2abe6f7aed0d4e3ab2e5
Awais Aslam <awaisaslam336@gmail.com>
2022-07-16 19:45:58 +0500

    Finding + Upserting Planets

--- server/src/model/planets.model.js
+++ server/src/model/planets.model.js
@@ -1,8 +1,7 @@
 const { parse } = require("csv-parse");
 const fs = require('fs');
 const path = require('path');
-
-const habitableplanets = [];
+const planets = require('./planets.mongo');
 
 function isHabitable(planet) {
     return planet['koi_disposition'] === 'CONFIRMED'
@@ -11,29 +10,43 @@ function isHabitable(planet) {
 }
 function loadPlanetsData() {
     return new Promise((resolve, reject) => {
-        fs.createReadStream(path.join(__dirname,'..','..','data','kepler_data.csv'))
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
             .pipe(parse({
                 comment: '#',
                 columns: true
             }))
-            .on('data', (data) => {
+            .on('data', async (data) => {
                 if (isHabitable(data)) {
-                    habitableplanets.push(data);
+                    savePlanet(data)
                 }
             })
             .on('error', (err) => {
                 console.log(err);
                 reject(err);
             })
-            .on('end', () => {
-                console.log(`${habitableplanets.length} habitable planets found!!!`);
+            .on('end', async () => {
+                const countPlanetsFound = (await getAllPlanets()).length;
+                console.log(`${countPlanetsFound} habitable planets found!!!`);
                 resolve();
             });
     });
 }
 
-function getAllPlanets(){
-    return habitableplanets;
+async function getAllPlanets() {
+    return await planets.find({});
+}
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name,
+        }, {
+            keplerName: planet.kepler_name,
+        }, {
+            upsert: true
+        })
+    } catch (err) {
+        console.log(`planet not saved! ${err}`);
+    }
 }
 
 module.exports = {
