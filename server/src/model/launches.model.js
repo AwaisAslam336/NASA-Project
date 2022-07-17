76ddf7c8d68b640f161eb765bccf97d0cb743028
Awais Aslam <awaisaslam336@gmail.com>
2022-07-17 21:02:24 +0500

    Adding Launches in Database/Auto-Increment mongodb

--- server/src/model/launches.model.js
+++ server/src/model/launches.model.js
@@ -1,32 +1,53 @@
 const launchesDatabase = require('./launches.mongo');
+const planetsDatabase = require('./planets.mongo');
 let launches = new Map();
-let newFlightNumber = 100;
-//example launch data
-let launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'kepler-442 b',
-    customers: ['NASA', 'ZTM'],
-    upcoming: true,
-    success: true
-}
+let DefaultFlightNumber = 100;
 
-launches.set(launch.flightNumber, launch);
+// //example launch data
+// let launch = {
+//     flightNumber: 100,
+//     mission: 'Kepler Exploration X',
+//     rocket: 'Explorer IS1',
+//     launchDate: new Date('December 27, 2030'),
+//     target: 'kepler-442 b',
+//     customers: ['NASA', 'ZTM'],
+//     upcoming: true,
+//     success: true
+// }
+
+async function saveLaunch(launch) {
+    const planet = await planetsDatabase.findOne({
+        keplerName: launch.target,
+    });
+    if(!planet){
+        throw new Error('No matching planet found');
+    }
+    await launchesDatabase.updateOne({flightNumber: launch.flightNumber}, launch, { upsert: true });
+}
 
 async function getAllLaunches() {
     return await launchesDatabase.find({}, { '_id': 0, '__v': 0 });
 }
 
-function addNewLaunch(launch) {
-    newFlightNumber++;
-    launches.set(newFlightNumber, Object.assign(launch, {
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase.findOne().sort('-flightNumber');
+    if (!latestLaunch) {
+        return DefaultFlightNumber;
+    }
+    return latestLaunch.flightNumber;
+}
+
+async function addNewLaunch(launch) {
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+
+    const newLaunch = Object.assign(launch, {
         flightNumber: newFlightNumber,
         customers: ['Easy Learning', 'NASA'],
         upcoming: true,
         success: true
-    }));
+    });
+
+    await saveLaunch(newLaunch);
 }
 
 function existsLaunchWithId(launchId) {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,7 +5,7 @@ async function httpGetAllLaunches(req, res) {
     return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
     if (!launch.mission || !launch.rocket || !launch.target || !launch.launchDate) {
@@ -21,7 +21,7 @@ function httpAddNewLaunch(req, res) {
         });
     }
 
-    addNewLaunch(launch)
+    await addNewLaunch(launch);
     return res.status(201).json(launch);
 }
 
