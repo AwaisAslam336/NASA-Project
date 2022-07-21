32953cd77188c62f930419a2b7f484978efb7ef7
Awais Aslam <awaisaslam336@gmail.com>
2022-07-21 15:39:51 +0500

    setting get method for all routers

--- server/src/app.js
+++ server/src/app.js
@@ -20,4 +20,8 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
 app.use(launchesRouter);
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 module.exports = app;
